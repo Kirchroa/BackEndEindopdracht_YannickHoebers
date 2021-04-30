538c148138759589201be50181e3bdbb57e69f58
Kirchroa <y.hoebers@novi-education.nl>
2021-04-30 20:21:53 +0200

    all dependencies added and basic communication with postgreSQL server working

---
+++ src/main/java/nl/novi/yannickhoebers/reversedrecipe/ReversedRecipeApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.yannickhoebers.reversedrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReversedRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReversedRecipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/novi/yannickhoebers/reversedrecipe/ReversedRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.yannickhoebers.reversedrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReversedRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
