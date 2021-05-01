8d618f1692e12632916fbfb7040e4d6770fe87f6
Kirchroa <y.hoebers@novi-education.nl>
2021-05-01 10:58:24 +0200

    Add model classes with entities and create relational database tables
    Add classes Recipe and Cook. Cook has a one-to-many relation with Recipe.
    Recipe has a many-to-one relation with Cook. Entities are converted to a relational
    database in PostgreSQL and are correctly shown.

--- src/main/java/nl/novi/yannickhoebers/reversedrecipe/model/Cook.java
+++ src/main/java/nl/novi/yannickhoebers/reversedrecipe/model/Cook.java
@@ -1,4 +1,76 @@
 package nl.novi.yannickhoebers.reversedrecipe.model;
 
-public class Cook {
+
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "cooks")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Cook implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+
+    @OneToMany(mappedBy = "cook", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Recipe> recipes;
+
+    public Cook() {}
+
+    public Cook(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+
+    public Set<Recipe> getAssignments(){
+        return this.recipes;
+    }
 }
