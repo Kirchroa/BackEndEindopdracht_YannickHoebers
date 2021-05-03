a3c00972f9ece3edc4cfc6786a9cbf188256061c
Kirchroa <y.hoebers@novi-education.nl>
2021-05-03 17:35:32 +0200

    Add get method with id selector for API request in the Cook Rest Controller
    Added @pathVariable annotation for targeting cook object with matching id. When the id is not present a not-found-exception will be thrown.

--- src/main/java/nl/novi/yannickhoebers/reversedrecipe/controller/CookController.java
+++ src/main/java/nl/novi/yannickhoebers/reversedrecipe/controller/CookController.java
@@ -2,10 +2,11 @@ package nl.novi.yannickhoebers.reversedrecipe.controller;
 
 import nl.novi.yannickhoebers.reversedrecipe.model.Cook;
 import nl.novi.yannickhoebers.reversedrecipe.repository.CookRepository;
+import nl.novi.yannickhoebers.reversedrecipe.service.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-
+import java.util.Optional;
 
 
 @RestController
@@ -18,5 +19,15 @@ public class CookController {
     @GetMapping("/cooks")
     public List<Cook> getAllCooks() {
         return cookRepository.findAll();
+    }
+
+    @GetMapping("/cook/{id}")
+    public Cook getCookByID(@PathVariable Long id) {
+        Optional<Cook> optCook = cookRepository.findById(id);
+        if(optCook.isPresent()) {
+            return optCook.get();
+        }else {
+            throw new NotFoundException("cook not found with id " + id);
+        }
     }
 }
