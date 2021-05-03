d25fe5cd87be08adf1349b103d1dd2e10042665c
Kirchroa <y.hoebers@novi-education.nl>
2021-05-03 17:29:19 +0200

    Added basic get method for API request in the Cook Rest Controller

--- src/main/java/nl/novi/yannickhoebers/reversedrecipe/controller/CookController.java
+++ src/main/java/nl/novi/yannickhoebers/reversedrecipe/controller/CookController.java
@@ -1,4 +1,22 @@
 package nl.novi.yannickhoebers.reversedrecipe.controller;
 
+import nl.novi.yannickhoebers.reversedrecipe.model.Cook;
+import nl.novi.yannickhoebers.reversedrecipe.repository.CookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+
+
+@RestController
+@RequestMapping("/api")
 public class CookController {
+
+    @Autowired
+    private CookRepository cookRepository;
+
+    @GetMapping("/cooks")
+    public List<Cook> getAllCooks() {
+        return cookRepository.findAll();
+    }
 }
