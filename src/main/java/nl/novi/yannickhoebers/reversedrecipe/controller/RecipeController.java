706427e347f986c94d5633584bedf1c854cfc6b2
Kirchroa <y.hoebers@novi-education.nl>
2021-05-04 12:53:34 +0200

    Add GetMapping annotation for Recipe REST Controller
    The mapping is directly linked with a Cook object. If the Cook object does not exist a not-found-exception will be thrown with an error message.

--- src/main/java/nl/novi/yannickhoebers/reversedrecipe/controller/RecipeController.java
+++ src/main/java/nl/novi/yannickhoebers/reversedrecipe/controller/RecipeController.java
@@ -1,4 +1,33 @@
 package nl.novi.yannickhoebers.reversedrecipe.controller;
 
+import nl.novi.yannickhoebers.reversedrecipe.model.Recipe;
+import nl.novi.yannickhoebers.reversedrecipe.repository.CookRepository;
+import nl.novi.yannickhoebers.reversedrecipe.repository.RecipeRepository;
+import nl.novi.yannickhoebers.reversedrecipe.service.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class RecipeController {
+    @Autowired
+    private RecipeRepository recipeRepository;
+
+    @Autowired
+    private CookRepository cookRepository;
+
+    @GetMapping("/cooks/{cookId}/recipes")
+    public List<Recipe> getContactByCookId(@PathVariable Long cookId) {
+
+        if(!cookRepository.existsById(cookId)) {
+            throw new NotFoundException("Cook not found!");
+        }
+
+        return recipeRepository.findByCookId(cookId);
+    }
 }
