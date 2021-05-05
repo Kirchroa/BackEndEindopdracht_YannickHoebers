433feb0b83229960742da0db1eb1f6a5af4ca789
Kirchroa <y.hoebers@novi-education.nl>
2021-05-05 22:32:08 +0200

    Add @CrossOrigin annotation to make communication with the front-end React application possible.
    The REST controllers are now able to communicate with the HTTP requests of the frond-end part of this application by accepting HTTP requests coming from http://localhost:3000/

--- src/main/java/nl/novi/yannickhoebers/reversedrecipe/controller/CookController.java
+++ src/main/java/nl/novi/yannickhoebers/reversedrecipe/controller/CookController.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class CookController {
 
     @Autowired
--- src/main/java/nl/novi/yannickhoebers/reversedrecipe/controller/RecipeController.java
+++ src/main/java/nl/novi/yannickhoebers/reversedrecipe/controller/RecipeController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000/")
 public class RecipeController {
     @Autowired
     private RecipeRepository recipeRepository;
